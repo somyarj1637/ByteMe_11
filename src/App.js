e1659c2ad2ab1af4b4132893f2357c3f09467161
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2022-06-26 16:16:47 +0530

    Final  Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Product from './Components/Product';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <Recomendation/> */}
+     
+      <Product />
+    </>
+  );
+}
+
+export default App;
